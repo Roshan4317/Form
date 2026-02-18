626c8b4f5b8c1bba0c5e4b0bc230565978536781
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-02-17 23:29:12 -0500

    Form Validation

--- components/ExpenseForm.jsx
+++ components/ExpenseForm.jsx
@@ -7,36 +7,57 @@ export default function ExpenseForm({ setExpenses }) {
     amount: "",
   });
 
-  const titleRef = useRef(null);
-  const categoryRef = useRef(null);
-  const amountRef = useRef(null);
+  const [errors, setErrors] = useState({});
+
+  // const titleRef = useRef(null);
+  // const categoryRef = useRef(null);
+  // const amountRef = useRef(null);
+
+  const validate = (formData) => {
+    const errorData = {};
+
+    if (!formData.title) {
+      errorData.title = "Title is Required";
+    }
+    if (!formData.category) {
+      errorData.category = "Please Select a Category";
+    }
+    if (!formData.amount) {
+      errorData.amount = "Please enter an amount";
+    }
+
+    setErrors(errorData);
+    return errorData;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // const expense = { ...getFormData(e), id: crypto.randomUUID() };
-    // setExpenses((prevState) => [...prevState, expense]);
-    // setExpenses((prevState) => [
-    //   ...prevState,
-    //   { ...expense, id: crypto.randomUUID() },
-    // ]);
-    // setExpense({
-    //   title: "",
-    //   category: "",
-    //   amount: "",
-    // });
+    const validateResult = validate(expense);
 
+    if (Object.keys(validateResult).length) return;
     setExpenses((prevState) => [
       ...prevState,
-      {
-        id: crypto.randomUUID(),
-        title: titleRef.current.value,
-        category: categoryRef.current.value,
-        amount: amountRef.current.value,
-      },
+      { ...expense, id: crypto.randomUUID() },
     ]);
-    titleRef.current.value = "";
-    categoryRef.current.value = "";
-    amountRef.current.value = "";
+
+    setExpense({
+      title: "",
+      category: "",
+      amount: "",
+    });
+
+    // setExpenses((prevState) => [
+    //   ...prevState,
+    //   {
+    //     id: crypto.randomUUID(),
+    //     title: titleRef.current.value,
+    //     category: categoryRef.current.value,
+    //     amount: amountRef.current.value,
+    //   },
+    // ]);
+    // titleRef.current.value = "";
+    // categoryRef.current.value = "";
+    // amountRef.current.value = "";
   };
 
   // function getFormData(e) {
@@ -49,6 +70,15 @@ export default function ExpenseForm({ setExpenses }) {
   //   return data;
   // }
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setExpense((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    setErrors({});
+  };
+
   return (
     <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
@@ -56,25 +86,28 @@ export default function ExpenseForm({ setExpenses }) {
         <input
           id="title"
           name="title"
-          ref={titleRef}
-          // value={expense.title}
+          // ref={titleRef}
+          value={expense.title}
           // onChange={(e) =>
           //   setExpense((prevState) => ({ ...prevState, title: e.target.value }))
           // }
+          onChange={handleChange}
         />
+        <p className="error">{errors.title}</p>
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
         <select
           name="category"
-          ref={categoryRef}
-          // value={expense.category}
+          // ref={categoryRef}
+          value={expense.category}
           // onChange={(e) =>
           //   setExpense((prevState) => ({
           //     ...prevState,
           //     category: e.target.value,
           //   }))
           // }
+          onChange={handleChange}
         >
           <option value="" hidden>
             Select Category
@@ -85,21 +118,24 @@ export default function ExpenseForm({ setExpenses }) {
           <option value="Education">Education</option>
           <option value="Medicine">Medicine</option>
         </select>
+        <p className="error">{errors.category}</p>
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
         <input
           id="amount"
           name="amount"
-          ref={amountRef}
-          // value={expense.amount}
+          // ref={amountRef}
+          value={expense.amount}
           // onChange={(e) =>
           //   setExpense((prevState) => ({
           //     ...prevState,
           //     amount: e.target.value,
           //   }))
           // }
+          onChange={handleChange}
         />
+        <p className="error">{errors.amount}</p>
       </div>
       <button className="add-btn">Add</button>
     </form>
