b064551d35d729d6e83f5be41c7abb04c1ad90f9
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-02-19 23:59:31 -0500

    Implemented Sorting Functionality

--- components/ExpensesTable.jsx
+++ components/ExpensesTable.jsx
@@ -20,6 +20,8 @@ export default function ExpensesTable({
 
   const [rowId, setRowId] = useState("");
 
+  const [callBack, setCallBack] = useState(() => () => {});
+
   const totalAmount = filteredData.reduce((acc, curr) => {
     return (acc += parseInt(curr.amount));
   }, 0);
@@ -35,10 +37,48 @@ export default function ExpensesTable({
         expenses={expenses}
         setIsEditing={setIsEditing}
       />
-      <table className="expense-table" onClick={() => setMenuPosition({})}>
+      <table
+        className="expense-table"
+        onClick={() => {
+          if (menuPosition.left) {
+            setMenuPosition({});
+          }
+        }}
+      >
         <thead>
           <tr>
-            <th>Title</th>
+            <th className="amount-column">
+              <div>
+                <span>Title</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  viewBox="0 0 384 512"
+                  className="arrow up-arrow"
+                  onClick={(e) =>
+                    setCallBack(() => (a, b) => a.title.localeCompare(b.title))
+                  }
+                >
+                  <title>Ascending</title>
+
+                  <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  viewBox="0 0 384 512"
+                  className="arrow down-arrow"
+                  onClick={(e) =>
+                    setCallBack(() => (a, b) => b.title.localeCompare(a.title))
+                  }
+                >
+                  <title>Descending</title>
+
+                  <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+                </svg>
+              </div>
+            </th>
+
             <th>
               <select onChange={(e) => setQuery(e.target.value)}>
                 <option value="">All</option>
@@ -57,8 +97,12 @@ export default function ExpensesTable({
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow up-arrow"
+                  onClick={(e) =>
+                    setCallBack(() => (a, b) => a.amount - b.amount)
+                  }
                 >
                   <title>Ascending</title>
+
                   <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
                 </svg>
                 <svg
@@ -66,8 +110,12 @@ export default function ExpensesTable({
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow down-arrow"
+                  onClick={(e) =>
+                    setCallBack(() => (a, b) => b.amount - a.amount)
+                  }
                 >
                   <title>Descending</title>
+
                   <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
                 </svg>
               </div>
@@ -75,24 +123,30 @@ export default function ExpensesTable({
           </tr>
         </thead>
         <tbody>
-          {filteredData.map(({ id, title, category, amount }) => (
-            <tr
-              key={id}
-              onContextMenu={(e) => {
-                e.preventDefault();
-                setMenuPosition({ left: e.clientX + 4, top: e.clientY + 4 });
-                setRowId(id);
-              }}
-            >
-              <td>{title}</td>
-              <td>{category}</td>
-              <td>₹{amount}</td>
-            </tr>
-          ))}
+          {filteredData
+            .sort(callBack)
+            .map(({ id, title, category, amount }) => (
+              <tr
+                key={id}
+                onContextMenu={(e) => {
+                  e.preventDefault();
+                  setMenuPosition({ left: e.clientX + 4, top: e.clientY + 4 });
+                  setRowId(id);
+                }}
+              >
+                <td>{title}</td>
+                <td>{category}</td>
+                <td>₹{amount}</td>
+              </tr>
+            ))}
 
           <tr>
             <th>Total</th>
-            <th></th>
+            <th>
+              <button onClick={() => setCallBack(() => () => {})}>
+                Clear Sort
+              </button>
+            </th>
             <th>₹{totalAmount}</th>
           </tr>
         </tbody>
